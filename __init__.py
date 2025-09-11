b6b2de0d7e7afe375f0a5793f51470a3d2d95649
allenmylath <allengeorgemylath@gmail.com>
2025-09-11 13:30:31 +0530

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,17 @@
+# File: kerala_police_crawler/__init__.py
+
+# This file makes Python treat the directory as a package
+
+
+# File: kerala_police_crawler/spiders/__init__.py
+
+# This file makes Python treat the directory as a package
+
+
+# File: scrapy.cfg
+
+[settings]
+default = kerala_police_crawler.settings
+
+[deploy]
+project = kerala_police_crawler
